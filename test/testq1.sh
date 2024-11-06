96442b7c7d0804a40d299da28f898c3cd15d6f0c
Tobias Ves Losada <tveslosada@itba.edu.ar>
2024-11-06 20:40:19 -0300

    test q1,q2

---
+++ test/testq1.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+print_error() {
+    echo -e "\e[31m$1\e[0m"
+    exit 1
+}
+
+echo "Query 1: Initializing server"
+cd ..
+cd ./bin/server
+./server.sh > /dev/null 2>&1 &
+
+
+PID=$!
+cd ../..
+
+sleep 6
+
+mkdir -p ./test/results
+
+echo "Query 1: Running client"
+
+[ -d ./bin/client ] || print_error "Client directory not found"
+cd ./bin/client
+
+./query1.sh -DinPath=../../test/data/ -DoutPath=../../test/results/ -Dcity=NYC -Daddresses=127.0.0.1:5701 > /dev/null 2>&1
+
+sleep 1
+
+cd ../..
+
+echo "Query 1: Killing server"
+
+if [ ! -z "$PID" ]; then
+    PPID_COL=$(ps aux | head -n 1 | tr -s ' ' '\n' | nl | grep -w "PPID" | awk '{print $1}')
+    PID_COL=$(ps aux | head -n 1 | tr -s ' ' '\n' | nl | grep -w "PID" | awk '{print $1}')
+    ps aux | awk -v pid="$PID" -v ppid_col="$PPID_COL" -v pid_col="$PID_COL" '{ if ($ppid_col == pid) print $pid_col }' | xargs -r kill
+    kill $PID
+
+else
+    echo "Process not found."
+fi
+sleep 5
+
+echo "Query 1: Testing results"
+
+diff -q ./test/results/query1.csv ./test/expected/expected-q1.csv >> /dev/null
+
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mQuery 1: TEST PASSED\e[0m"
+else
+    echo -e "\e[31mQuery 1: FAILED\e[0m"
+fi
